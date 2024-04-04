731f6ad22c8b144f1888fb681a67ffe2d97d3dce
Revant Tripathi <revanttripathi8@gmail.com>
2024-04-04 22:55:26 +0530

    image

--- src/profile/Profile.js
+++ src/profile/Profile.js
@@ -1,28 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Profile.css'
 import logo from '../assests/logo.jpg'
+import pic from '../assests/picture.jpg'
+
 function Profile() {
+    const [image, setImage] = useState('');
+
+    const handleImageChange = (event) => {
+        const file = event.target.files[0];
+        console.log(file)
+        setImage(event.target.files[0])
+    }
     return (
         <>
             <div className='profile'>
                 <div className='logo-d'>
                     <img src={logo}></img>
+
                 </div>
                 <div className='form-section'>
                     <div className='welcome-text'>
                         <h1>Welcome! Let's create your profile</h1>
-                       <span className='gg'> <p>Let others get to know you better! You can do these later</p></span>
+                        <span className='gg'> <p>Let others get to know you better! You can do these later</p></span>
                     </div>
                     <div className='avt-text'>
                         <h3>Add an avatar</h3>
                     </div>
                     <div className='picture-sec'>
                         <div>
-                            <img className='avt' src='' alt=''></img>
+                            {/* <img className='avt' src={pic} alt=''></img> */}
+                            {image ? <img className='avt' src={URL.createObjectURL(image)} /> :
+                                <img className='avt'src={pic}></img>
+                            }
                         </div>
                         <div className='bt'>
                             <div className='upload-bt'>
-                                <input type='file' />
+                                <input type='file' onChange={handleImageChange} />
                             </div>
                         </div>
 
